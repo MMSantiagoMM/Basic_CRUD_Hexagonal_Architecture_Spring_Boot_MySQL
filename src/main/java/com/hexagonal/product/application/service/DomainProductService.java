27ad7717534bb3bea0d9df3adfce630f09e62f48
Santiago Mosquera Mapura <s.mosquera@globant.com>
2023-11-28 18:33:31 -0500

    setting up configurations to database deployed in the cloud

--- src/main/java/com/hexagonal/product/application/service/DomainProductService.java
+++ src/main/java/com/hexagonal/product/application/service/DomainProductService.java
@@ -3,21 +3,19 @@ package com.hexagonal.product.application.service;
 import com.hexagonal.product.application.dto.ProductDTO;
 import com.hexagonal.product.application.mapper.ProductMapperDTO;
 import com.hexagonal.product.domain.model.Product;
-import com.hexagonal.product.domain.port.ProductRepository;
 import com.hexagonal.product.infrastructure.adapter.ProductRepositoryMySQL;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class DomainProductService implements ProductService{
 
 
     private final ProductRepositoryMySQL productRepositoryMySQL;
+    private final ProductMapperDTO productMapperDTO;
 
-    public DomainProductService(ProductRepositoryMySQL productRepositoryMySQL) {
+    public DomainProductService(ProductRepositoryMySQL productRepositoryMySQL, ProductMapperDTO productMapperDTO) {
         this.productRepositoryMySQL = productRepositoryMySQL;
+        this.productMapperDTO = productMapperDTO;
     }
 
 
@@ -34,7 +32,7 @@ public class DomainProductService implements ProductService{
     @Override
     public Product saveProduct(ProductDTO product) {
         return productRepositoryMySQL.saveProduct(
-                ProductMapperDTO.INSTANCE.productDtoToProduct(product)
+                productMapperDTO.productDtoToProduct(product)
         );
     }
 
